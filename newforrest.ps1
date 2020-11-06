d1bc938bbbc3953196c396fad803688239d17ffb
Andreas Ihlefeld <win10trainer@outlook.de>
2020-11-06 13:05:27 +0100

    Erste Version

---
+++ newforrest.ps1
@@ -0,0 +1,8 @@
+﻿Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+$pwd = ConvertTo-SecureString 'Pa55w.rd1234' -AsPlainText -Force
+$dom = 'seminar.local'
+$domnetbios = $dom.Split('\.')[0]
+$pathes = 'C:\windows\ntds'
+
+Install-ADDSForest -SkipPreChecks -DomainName $dom -SafeModeAdministratorPassword $pwd -DatabasePath $pathes -DomainNetbiosName $domnetbios -DomainMode WinThreshold -LogPath $pathes -ForestMode WinThreshold -SysvolPath $pathes
